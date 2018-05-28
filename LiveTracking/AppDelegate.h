2fc19d9c11d50216851ea275d0ba5e7e3ba42037
Rajesh Karmaker <rajeshkarmaker0@gmail.com>
2018-05-28 21:29:56 +0600

    Initial Commit

---
+++ LiveTracking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveTracking
+//
+//  Created by Rajesh Karmaker on 28/5/18.
+//  Copyright © 2018 Rajesh Karmaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveTracking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveTracking
+//
+//  Created by Rajesh Karmaker on 28/5/18.
+//  Copyright © 2018 Rajesh Karmaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
